3bcf5c9000fdcc0087800fa45590ce95eafb19af
Eric Skroch <eskroch@mac.com>
2012-03-13 21:37:43 -0400

    Added printing support in CPTGraphHostingView on OS X based on code from Craig Hockenberry. Added printing example to CPTTestApp.

--- documentation/html/MacOS/interface_c_p_t_graph_hosting_view.js
+++ documentation/html/MacOS/interface_c_p_t_graph_hosting_view.js
@@ -1,4 +1,5 @@
 var interface_c_p_t_graph_hosting_view =
 [
-    [ "hostedGraph", "interface_c_p_t_graph_hosting_view.html#a009ea355bc684c3872c98e460b89f644", null ]
+    [ "hostedGraph", "interface_c_p_t_graph_hosting_view.html#a009ea355bc684c3872c98e460b89f644", null ],
+    [ "printRect", "interface_c_p_t_graph_hosting_view.html#aa5f853281eb400f940a0440f7ec171d9", null ]
 ];
