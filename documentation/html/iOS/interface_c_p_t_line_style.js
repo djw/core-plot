7b0251b9bc7b423fe17c22f8dced76dfa286af7a
Eric Skroch <eskroch@mac.com>
2012-03-26 21:37:51 -0400

    Added a line fill property to `CPTLineStyle`. Fixed issue 411.

--- documentation/html/iOS/interface_c_p_t_line_style.js
+++ documentation/html/iOS/interface_c_p_t_line_style.js
@@ -2,9 +2,12 @@ var interface_c_p_t_line_style =
 [
     [ "lineStyle", "interface_c_p_t_line_style.html#a1a62163fa497ad6a5eeb2d31e654424d", null ],
     [ "setLineStyleInContext:", "interface_c_p_t_line_style.html#a27d395a192f79ba1031afa479449cb4e", null ],
+    [ "strokePathInContext:", "interface_c_p_t_line_style.html#a31cb12062448c32ddcaa3b3dde1359e7", null ],
+    [ "strokeRect:inContext:", "interface_c_p_t_line_style.html#aba2c51249ad491853ca896a76633236f", null ],
     [ "dashPattern", "interface_c_p_t_line_style.html#a6053a92df9fbf4f49fd9c727efca2f94", null ],
     [ "lineCap", "interface_c_p_t_line_style.html#aa2010258cc8e768ac59954a8b7d8f3f0", null ],
     [ "lineColor", "interface_c_p_t_line_style.html#abbbc1a4147e36b4473da4ed9859ccd9a", null ],
+    [ "lineFill", "interface_c_p_t_line_style.html#a8c3a86e4d6154458777bb280e3a6485d", null ],
     [ "lineJoin", "interface_c_p_t_line_style.html#a5b41674be302ef1801d33ce079da3b42", null ],
     [ "lineWidth", "interface_c_p_t_line_style.html#ad742282d977c6ec6cd251b2cd8f7c1de", null ],
     [ "miterLimit", "interface_c_p_t_line_style.html#a7fba8edf2d131f6baad56635381a946b", null ],
