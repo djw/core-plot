ddb2328ee723e4e2edd210d53c82bc4186f44470
Eric Skroch <eskroch@mac.com>
2012-04-26 21:27:17 -0400

    Added an end angle property to CPTPieChart. Fixed issue 404.

--- documentation/html/MacOS/interface_c_p_t_pie_chart.js
+++ documentation/html/MacOS/interface_c_p_t_pie_chart.js
@@ -5,6 +5,7 @@ var interface_c_p_t_pie_chart =
     [ "pointingDeviceDownEvent:atPoint:", "interface_c_p_t_pie_chart.html#ac6d95068f152d9e5d8aa076443859544", null ],
     [ "borderLineStyle", "interface_c_p_t_pie_chart.html#ad42ce799d20617a6c9a8879aea31fbb3", null ],
     [ "centerAnchor", "group__plot_animation_pie_chart.html#ga4c665d1eb7b562f8ced08a62d980ee2c", null ],
+    [ "endAngle", "group__plot_animation_pie_chart.html#ga1ec146fb288f43545adfd617014d7c94", null ],
     [ "overlayFill", "interface_c_p_t_pie_chart.html#a25deafa6f8e629789652c90789f19b51", null ],
     [ "pieInnerRadius", "group__plot_animation_pie_chart.html#gaaf477318b2ce1ad016c532d386b5379e", null ],
     [ "pieRadius", "group__plot_animation_pie_chart.html#ga6bae24e928917347230bab548418ceea", null ],
