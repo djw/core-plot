cf640a8ae66a245c55365ae07580ab5de9347175
Eric Skroch <eskroch@mac.com>
2012-04-28 16:19:17 -0400

    Added the -didFinishDrawing: delegate method to CPTPlot. Fixed issue 368.

--- documentation/html/MacOS/protocol_c_p_t_plot_delegate-p.js
+++ documentation/html/MacOS/protocol_c_p_t_plot_delegate-p.js
@@ -1,4 +1,5 @@
 var protocol_c_p_t_plot_delegate-p =
 [
+    [ "didFinishDrawing:", "protocol_c_p_t_plot_delegate-p.html#af68ded0e9746646e581c32b1aa999573", null ],
     [ "plot:dataLabelWasSelectedAtRecordIndex:", "protocol_c_p_t_plot_delegate-p.html#ad6ca9ed72b54b2715f01c95d086eb78b", null ]
 ];
