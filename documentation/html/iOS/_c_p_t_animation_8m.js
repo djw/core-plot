bb44367ffd67dd358878fb939ea738eaa55a62ac
Eric Skroch <eskroch@mac.com>
2013-09-07 13:48:41 -0400

    Removed unneeded documentation files.

--- documentation/html/iOS/_c_p_t_animation_8m.js
+++
@@ -1,5 +0,0 @@
-var _c_p_t_animation_8m =
-[
-    [ "instance", "_c_p_t_animation_8m.html#a6ebbe922abbf7dbaf67a5589620467c5", null ],
-    [ "kCPTAnimationFrameRate", "_c_p_t_animation_8m.html#add5be08071f3ce1c17ae81245d751e8e", null ]
-];
